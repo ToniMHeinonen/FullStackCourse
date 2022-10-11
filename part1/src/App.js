00caa997fb5330478aefa003432a68ad8aa8bad2
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-11 12:33:19 +0300

    Render root element every second
    Show number of renders with counter.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,27 +1,8 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born {bornYear()}</p>
-    </>
+    <div>{counter}</div>
   )
 }
 
-const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
-    </>
-  )
-}
-
-export default App;
+export default App
--- part1/src/index.js
+++ part1/src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let counter = 1
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const refresh = () => {
+    root.render(<App counter={counter} />)
+}
+
+setInterval(() => {
+    refresh()
+    counter += 1
+}, 1000)
