90557e7c326343b2199385de94e6894936d8177c
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-11 12:56:30 +0300

    Complete part 1c code
    Practice using states.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,29 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from 'react'
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({handleClick, text}) => {
   return (
-    <div>{counter}</div>
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text='plus' />
+      <Button handleClick={setToZero} text='zero' />
+      <Button handleClick={decreaseByOne} text='minus' />
+    </div>
   )
 }
 
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import App from './App';
+import App from './App'
 
-let counter = 1
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const refresh = () => {
-    root.render(<App counter={counter} />)
-}
-
-setInterval(() => {
-    refresh()
-    counter += 1
-}, 1000)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
