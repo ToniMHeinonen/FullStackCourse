e4665e1e7b9d9062289490a3985b9b7cb4418f57
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-12 12:53:53 +0300

    Complete exercise 1.7
    Add all, average and positive percentage values.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,6 +13,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+  const average = ((good * 1) + (neutral * 0) + (bad * -1)) / all
+  const positive = good / all * 100 + " %"
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -23,6 +27,9 @@ const App = () => {
       <Stat name="good" amount={good} />
       <Stat name="neutral" amount={neutral} />
       <Stat name="bad" amount={bad} />
+      <Stat name="all" amount={all} />
+      <Stat name="average" amount={average} />
+      <Stat name="positive" amount={positive} />
     </div>
   )
 }
