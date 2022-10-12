53df43da5f8519973c0a83aa8bfd37a0be4e630f
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-12 13:46:26 +0300

    Use Prettier code formatter on project

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
-)
+);
 
 const StatisticLine = ({ text, value }) => (
   <tr>
-    <td><p>{text}</p></td>
-    <td><p>{value}</p></td>
+    <td>
+      <p>{text}</p>
+    </td>
+    <td>
+      <p>{value}</p>
+    </td>
   </tr>
-)
+);
 
 const Statistics = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad
+  const all = good + neutral + bad;
 
   if (all == 0) {
     return (
@@ -20,18 +24,18 @@ const Statistics = ({ good, neutral, bad }) => {
         <h1>statistics</h1>
         <p>No feedback given</p>
       </>
-    )
+    );
   }
 
-  const average = ((good * 1) + (neutral * 0) + (bad * -1)) / all
-  const positive = good / all * 100 + " %"
+  const average = (good * 1 + neutral * 0 + bad * -1) / all;
+  const positive = (good / all) * 100 + " %";
 
   return (
     <>
       <h1>statistics</h1>
       <table>
         <colgroup>
-          <col style={{ width: "50px"}} />
+          <col style={{ width: "50px" }} />
         </colgroup>
         <StatisticLine text="good" value={good} />
         <StatisticLine text="neutral" value={neutral} />
@@ -41,17 +45,17 @@ const Statistics = ({ good, neutral, bad }) => {
         <StatisticLine text="positive" value={positive} />
       </table>
     </>
-  )
-}
+  );
+};
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const all = good + neutral + bad
-  const average = ((good * 1) + (neutral * 0) + (bad * -1)) / all
-  const positive = good / all * 100 + " %"
+  const all = good + neutral + bad;
+  const average = (good * 1 + neutral * 0 + bad * -1) / all;
+  const positive = (good / all) * 100 + " %";
 
   return (
     <div>
@@ -61,7 +65,7 @@ const App = () => {
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
