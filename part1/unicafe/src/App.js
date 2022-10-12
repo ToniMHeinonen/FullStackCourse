11a02146a45635fe0c3819b987d5b06e15d0b66b
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-12 12:58:20 +0300

    Complete exercise 1.8
    - Add Statistics component.
    - Move statistics related code from App to Statistics.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,6 +8,24 @@ const Stat = ({ name, amount }) => (
   <p>{name} {amount}</p>
 )
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = ((good * 1) + (neutral * 0) + (bad * -1)) / all
+  const positive = good / all * 100 + " %"
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <Stat name="good" amount={good} />
+      <Stat name="neutral" amount={neutral} />
+      <Stat name="bad" amount={bad} />
+      <Stat name="all" amount={all} />
+      <Stat name="average" amount={average} />
+      <Stat name="positive" amount={positive} />
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -23,13 +41,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <h1>statistics</h1>
-      <Stat name="good" amount={good} />
-      <Stat name="neutral" amount={neutral} />
-      <Stat name="bad" amount={bad} />
-      <Stat name="all" amount={all} />
-      <Stat name="average" amount={average} />
-      <Stat name="positive" amount={positive} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
