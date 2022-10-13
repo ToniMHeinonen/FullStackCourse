e313c5ef9e8497b18090fbe479cf8861ee194ef5
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-13 13:38:58 +0300

    Add kurssitiedot react app to part2 directory
    - Copy App.js and index.js files from part1 "kurssitiedot" directory to newly created app.

---
+++ part2/kurssitiedot/src/App.js
@@ -0,0 +1,54 @@
+const Header = (props) => <h1>{props.course.name}</h1>;
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  let total = 0;
+  const t = props.parts.forEach((value) => (total += value.exercises));
+
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
