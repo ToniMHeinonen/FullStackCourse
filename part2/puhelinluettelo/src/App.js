ce3b4850974fcf38a201af05b4930eb76d29715d
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-14 13:06:51 +0300

    Complete exercise 2.6
    Practice adding text from input to arrays.

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -4,19 +4,32 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addName = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+  };
+
+  const handleInput = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInput} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <>
+        {persons.map((p) => (
+          <div key={p.name}>{p.name}</div>
+        ))}
+      </>
     </div>
   );
 };
