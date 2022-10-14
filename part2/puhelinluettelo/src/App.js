2ab78a089b9d9db7c53c119b495df5b6788949fd
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-14 13:21:39 +0300

    Complete exercise 2.7
    Prevent adding duplicate name values.

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -6,6 +6,12 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault();
+
+    if (persons.map((p) => p.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
     setPersons(persons.concat({ name: newName }));
   };
 
