5a4fcf059eed9999b4a2363e8a65e44f0a3f9d2a
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-14 14:56:30 +0300

    Add database examples to part 2
    - Add db.json file, which holds all the notes.
    - Import axios package.
    - Install JSON Server as a development dependency by running command "npm install json-server --save-dev".
    - Add line "server": "json-server -p3001 --watch db.json" to package.json scripts in order to make running server easier, by running command "npm run server".
    - Retrieve notes from database in App.js by using effect and axios.

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,38 +1,48 @@
-import { useState } from "react";
-import Note from "./components/Note";
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
-  const [newNote, setNewNote] = useState("a new note...");
-  const [showAll, setShowAll] = useState(true);
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes').then((response) => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
       id: notes.length + 1,
-    };
+    }
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
-  };
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value);
-    setNewNote(event.target.value);
-  };
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? "important" : "all"}
+          show {showAll ? 'important' : 'all'}
         </button>
       </div>
       <ul>
@@ -45,7 +55,7 @@ const App = (props) => {
         <button type="submit">save</button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
