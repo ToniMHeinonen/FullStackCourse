6d78b5f0db22d8863fda4280e16e59b78be31598
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-17 11:21:00 +0300

    Complete exercise 2.14
    - Show weather data for countries by using Open Weather Map API (Launch program with command "set "REACT_APP_API_KEY={YOUR_API_KEY}" && npm start" to make it work on Windows).
    - Add CountryInfo and WeatherInfo components.
    - Display "Loading weather data" if weather has not been loaded.

--- part2/maiden_tiedot/src/App.js
+++ part2/maiden_tiedot/src/App.js
@@ -7,19 +7,7 @@ const DisplayCountry = ({country}) => {
   return (
     <>
       {showInfo ? (
-        <div>
-          <h1>{country.name.common}</h1>
-          <button onClick={() => setShowInfo(false)}>Hide</button>
-          <div>capital {country.capital[0]}</div>
-          <div>area {country.area}</div>
-          <h2>languages:</h2>
-          <ul>
-            {Object.keys(country.languages).map((key) => (
-              <li key={key}>{country.languages[key]}</li>
-            ))}
-          </ul>
-          <img src={country.flags.png} alt="Country  flag" width="150" />
-        </div>
+        <CountryInfo country={country} setShowInfo={setShowInfo} />
       ) : (
         <div>
           {country.name.common}
@@ -30,12 +18,70 @@ const DisplayCountry = ({country}) => {
   )
 }
 
+const CountryInfo = ({country, setShowInfo}) => {
+  const [weather, setWeather] = useState({})
+
+  const api_key = process.env.REACT_APP_API_KEY
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${country.latlng[0]}&lon=${country.latlng[1]}&appid=${api_key}`
+      )
+      .then((response) => setWeather(response.data))
+  }, [])
+
+  const capital = country.capital[0]
+
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <button onClick={() => setShowInfo(false)}>Hide</button>
+      <div>capital {capital}</div>
+      <div>area {country.area}</div>
+      <h2>languages:</h2>
+      <ul>
+        {Object.keys(country.languages).map((key) => (
+          <li key={key}>{country.languages[key]}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt="Country  flag" width="150" />
+      <h2>Weather in {capital}</h2>
+      <WeatherInfo weather={weather} />
+    </div>
+  )
+}
+
+const WeatherInfo = ({weather}) => {
+  const kelvinToCelcius = -273.15
+  const weatherLoaded = Object.keys(weather).length !== 0
+
+  return (
+    <>
+      {weatherLoaded ? (
+        <div>
+          <div>
+            temperature {(weather.main.temp + kelvinToCelcius).toFixed(2)}{' '}
+            celsius
+          </div>
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={`Weather icon, ${weather.weather.description}`}
+          />
+          <div>wind {weather.wind?.speed} m/s</div>{' '}
+        </div>
+      ) : (
+        <div>Loading weather info</div>
+      )}
+    </>
+  )
+}
+
 function App() {
   const [filter, setFilter] = useState('')
   const [countries, setCountries] = useState([])
 
   useEffect(() => {
-    console.log('getting')
     axios
       .get('https://restcountries.com/v3.1/all')
       .then((response) => setCountries(response.data))
@@ -57,7 +103,9 @@ function App() {
       {showCountries.length > 10 ? (
         <div>Too many matches, specify another filter</div>
       ) : (
-        showCountries.map((c) => <DisplayCountry country={c} />)
+        showCountries.map((c) => (
+          <DisplayCountry key={c.name.common} country={c} />
+        ))
       )}
     </>
   )
