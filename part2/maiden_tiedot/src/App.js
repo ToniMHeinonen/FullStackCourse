12e76153768d56bf64177e8ba097985432587c3f
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-17 09:40:25 +0300

    Complete exercise 2.12
    - Import axios package.
    - Fetch all countries from RESTCountries API.
    - Filter countries by name.
    - Display more info about the country when filter displays only one country.

--- part2/maiden_tiedot/src/App.js
+++ part2/maiden_tiedot/src/App.js
@@ -1,5 +1,54 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const DisplayCountry = ({country}) => (
+  <div>
+    <h1>{country.name.common}</h1>
+    <div>capital {country.capital[0]}</div>
+    <div>area {country.area}</div>
+    <h2>languages:</h2>
+    <ul>
+      {Object.keys(country.languages).map((key) => (
+        <li key={key}>{country.languages[key]}</li>
+      ))}
+    </ul>
+    <img src={country.flags.png} alt="Country  flag" width="150" />
+  </div>
+)
+
 function App() {
-  return <div></div>
+  const [filter, setFilter] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    console.log('getting')
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then((response) => setCountries(response.data))
+  }, [])
+
+  const handleFilterInput = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const showCountries = countries.filter((c) =>
+    c.name.common.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <>
+      <div>
+        find countries <input value={filter} onChange={handleFilterInput} />
+      </div>
+      {showCountries.length > 10 ? (
+        <div>Too many matches, specify another filter</div>
+      ) : showCountries.length == 1 ? (
+        <DisplayCountry country={showCountries[0]} />
+      ) : (
+        showCountries.map((c) => <div>{c.name.common}</div>)
+      )}
+    </>
+  )
 }
 
 export default App
