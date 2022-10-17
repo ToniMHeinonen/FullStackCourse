72174ecd1cb9fd3598193595e1fa58060e2b782a
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-17 13:50:21 +0300

    Add css examples to part 2
    - Use inline styles and css file.
    - Add footer and notification components.

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,10 +2,36 @@ import {useState, useEffect} from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
 
+const Notification = ({message}) => {
+  if (message === null) {
+    return null
+  }
+
+  return <div className="error">{message}</div>
+}
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16,
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2022
+      </em>
+    </div>
+  )
+}
+
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -23,7 +49,12 @@ const App = () => {
         setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`)
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter((n) => n.id !== id))
       })
   }
@@ -52,6 +83,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -70,6 +102,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
--- part2/src/index.js
+++ part2/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import axios from 'axios'
+import './index.css'
 
 import App from './App'
 
