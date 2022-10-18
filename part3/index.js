59444cd9314843ca4dbb50fb04f6d7354f94fc1d
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-18 10:45:00 +0300

    Add middleware examples to part 3
    - Add requestLogger(), which logs information about requests.
    - Add unknownEndpoint(), which shows error page to user if request fails.

--- part3/index.js
+++ part3/index.js
@@ -3,6 +3,16 @@ const app = express()
 
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
 let notes = [
   {
     id: 1,
@@ -75,6 +85,12 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
