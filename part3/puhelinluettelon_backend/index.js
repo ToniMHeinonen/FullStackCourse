3299bef251268a9a69d168eb0c7938b758e6e25b
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-18 10:58:46 +0300

    Complete exercise 3.8
    Display body in morgan if body is not empty.

--- part3/puhelinluettelon_backend/index.js
+++ part3/puhelinluettelon_backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+morgan.token('body', function (req, res) {
+  return Object.keys(req.body).length != 0 ? JSON.stringify(req.body) : ' '
+})
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 let persons = [
   {
