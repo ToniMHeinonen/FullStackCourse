31b9a322f5657ca0f296025c98e22d0bfe51d2fc
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-18 13:40:34 +0300

    Deploy part 3 and part 2 code to fly.io
    - Make part 2 react app work with local backend and in fly.io backend by changing baseUrl and defining proxy in package.json.
    - Import cors package to part 3.
    - Add scripts to part 3 package.json for easier deployment and fly.io logging.
    - Create development build from part 2 and copy it in part 3 backend project.

--- part2/src/App.js
+++ part2/src/App.js
@@ -31,7 +31,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
+
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -92,7 +96,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
