771d715e4ed5f118969cac1c1c617fffcecf37d5
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-20 10:28:55 +0300

    Add examples for modifying database data to part 3
    - Make deleting data functional by using findByIdAndRemove().
    - Make modifying data functional by using findByIdAndUpdate().
    - Add errorHandler() middleware and make it handle cast errors when retrieving id from MongoDB.
    - Add requests for deleting note and getting note by id.

--- part3/index.js
+++ part3/index.js
@@ -50,17 +50,24 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter((note) => note.id !== id)
-
-  response.status(204).end()
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -83,11 +90,41 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    .then((updatedNote) => {
+      response.json(updatedNote)
+    })
+    .catch((error) => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({error: 'unknown endpoint'})
 }
 
+// This needs to be added after every HTTP request, otherwise it blocks them
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    console.log('cast')
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+// This needs to be added after every other middleware
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
