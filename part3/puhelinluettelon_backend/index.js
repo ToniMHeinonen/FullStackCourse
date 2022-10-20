d05fd894a22f35b3b8ed0b9cf4dd899918337810
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-20 10:33:15 +0300

    Complete exercise 3.15
    Make deleting persons functional with MongoDB.

--- part3/puhelinluettelon_backend/index.js
+++ part3/puhelinluettelon_backend/index.js
@@ -57,10 +57,14 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((person) => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => {
+      console.log(error)
+      response.status(400).send({error: 'malformatted id'})
+    })
 })
 
 app.post('/api/persons', (request, response) => {
