df02141539e342682889ece1fd77f0f91fb97086
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-21 13:24:31 +0300

    Add part 3 examples for mongoose validation
    - Add validation for note content and date in note.js.
    - Remove manual validation from index.js and use the built in validator from mongoose.
    - Add a request for updating a note.

--- part3/index.js
+++ part3/index.js
@@ -70,14 +70,8 @@ app.delete('/api/notes/:id', (request, response, next) => {
     .catch((error) => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
-    })
-  }
 
   const note = new Note({
     content: body.content,
@@ -85,20 +79,22 @@ app.post('/api/notes', (request, response) => {
     date: new Date(),
   })
 
-  note.save().then((savedNote) => {
-    response.json(savedNote)
-  })
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote)
+    })
+    .catch((error) => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const {content, important} = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+  Note.findByIdAndUpdate(
+    request.params.id,
+    {content, important},
+    {new: true, runValidators: true, context: 'query'}
+  )
     .then((updatedNote) => {
       response.json(updatedNote)
     })
@@ -118,6 +114,8 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     console.log('cast')
     return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
   }
 
   next(error)
