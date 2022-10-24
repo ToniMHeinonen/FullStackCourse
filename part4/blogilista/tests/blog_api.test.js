104d6d9da035bc04c17e088afab0c19c6ef3ffb2
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-24 12:49:40 +0300

    Complete exercise 4.8
    - Import cross-env and supertest packages.
    - Modify start, dev and test scripts to set production environments.
    - Modify test script to run tests one at a time and to force close the test when running mongoose tests.
    - Use async/await in blogs.js controller.
    - Add blog_api test file for testing async api callbacks.
    - Add test_helper.js file which contains helpful variables and functions used for testing.

---
+++ part4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,29 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
---
+++ part4/blogilista/tests/test_helper.js
@@ -0,0 +1,18 @@
+const initialBlogs = [
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 3,
+  },
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 10,
+  },
+]
+
+module.exports = {
+  initialBlogs,
+}
