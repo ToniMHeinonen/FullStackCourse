18c7f6f4caaabd3f621772809d90812bd498ce47
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-24 13:08:14 +0300

    Complete exercise 4.10
    - Convert post() to async function in blogs.js controller.
    - Add test for adding a new blog.

--- part4/blogilista/tests/blog_api.test.js
+++ part4/blogilista/tests/blog_api.test.js
@@ -30,6 +30,27 @@ test('blog has id field', async () => {
   expect(blogs[0].id).toBeDefined()
 })
 
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'Test title',
+    author: 'Test author',
+    url: 'Test url',
+    likes: 10,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map((b) => b.title)
+  expect(titles).toContain('Test title')
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
