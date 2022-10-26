eeb560a1486e8feac6b839ece121b2f122d3182d
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-26 10:34:33 +0300

    Complete exercise 4.17
    - Show more information about user when retrieving blogs.
    - Add blogs field to User.
    - Show written blogs when retrieving users.
    - Fix tests using wrong MONGODB_URI.

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,22 +1,32 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const users = await User.find({})
+  const user = users[0]
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user.id,
   })
 
+  console.log(blog)
+
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
--- part4/blogilista/models/user.js
+++ part4/blogilista/models/user.js
@@ -5,6 +5,12 @@ const userSchema = mongoose.Schema({
   username: { type: String, unique: true, minLength: 3 },
   name: String,
   passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog',
+    },
+  ],
 })
 
 userSchema.set('toJSON', {
