ef1919a6cb6c8913f100b675efe48b779c81239e
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-27 09:14:23 +0300

    Complete exercise 4.19
    - Use token authorization when creating new blogs.
    - Add authorization token field to create_blog.rest.

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -10,8 +19,13 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const users = await User.find({})
-  const user = users[0]
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
@@ -20,8 +34,6 @@ blogsRouter.post('/', async (request, response) => {
     likes: body.likes || 0,
     user: user.id,
   })
-
-  console.log(blog)
 
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog._id)
