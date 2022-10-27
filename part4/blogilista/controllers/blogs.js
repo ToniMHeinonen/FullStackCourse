ba4282986e69c4a4031a726ea69f0c3468d92ec2
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-27 10:23:22 +0300

    Complete exercise 4.22
    - Add userExtractor() middleware, which retrieves user from token.
    - Use userExtractor() when posting and deleting blogs.

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,22 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   const body = request.body
-
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!request.token || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   const blog = new Blog({
     title: body.title,
@@ -33,21 +26,22 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!request.token || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
+blogsRouter.delete(
+  '/:id',
+  middleware.userExtractor,
+  async (request, response) => {
+    const user = request.user
+
+    const blog = await Blog.findById(request.params.id)
+
+    if (blog.user.toString() !== user.id) {
+      return response.status(401).json({ error: 'user has no access to blog' })
+    }
+
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
   }
-
-  const blog = await Blog.findById(request.params.id)
-
-  if (blog.user.toString() !== decodedToken.id) {
-    return response.status(401).json({ error: 'user has no access to blog' })
-  }
-
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
-})
+)
 
 blogsRouter.put('/:id', async (request, response, next) => {
   const body = request.body
--- part4/blogilista/utils/middleware.js
+++ part4/blogilista/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
@@ -6,6 +8,12 @@ const tokenExtractor = (request, response, next) => {
     request.token = authorization.substring(7)
   }
 
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  request.user = await User.findById(decodedToken.id)
   next()
 }
 
@@ -24,4 +32,5 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   errorHandler,
   tokenExtractor,
+  userExtractor,
 }
