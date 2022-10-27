81714e19afde8814350b2a2c04d8fedf2c599754
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-10-27 09:09:05 +0300

    Complete exercise 4.18
    - Add token based login.
    - Import jsonwebtoken package.
    - Add login.js controller.
    - Add request for logging in.

--- part4/blogilista/app.js
+++ part4/blogilista/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const notesRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI)
@@ -15,6 +16,7 @@ app.use(express.json())
 
 app.use('/api/blogs', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
