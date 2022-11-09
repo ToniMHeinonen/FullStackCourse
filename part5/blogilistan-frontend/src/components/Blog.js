7bd6e3304174abfb63ba0d8fe6c6ed6c080f1653
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-09 11:27:48 +0200

    Complete exercise 5.7
    - Add ability to show and hide more information on blogs.
    - Add some inline css style to blogs.

--- part5/blogilistan-frontend/src/components/Blog.js
+++ part5/blogilistan-frontend/src/components/Blog.js
@@ -1,7 +1,38 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      {visible ? (
+        <>
+          <div onClick={() => setVisible(false)}>
+            {blog.title} {blog.author}{' '}
+            <button onClick={() => setVisible(false)}>hide</button>
+          </div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.user ? blog.user.name : 'Undefined author'}</div>
+        </>
+      ) : (
+        <div onClick={() => setVisible(true)}>
+          {blog.title} {blog.author}{' '}
+          <button onClick={() => setVisible(true)}>show</button>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
