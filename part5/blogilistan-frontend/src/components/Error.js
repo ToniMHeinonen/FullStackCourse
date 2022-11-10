7d32c4938e65e3d64aa89b734370b3ee65c26874
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-10 10:51:23 +0200

    Complete exercise 5.12
    - Add ESLint configuration files.
    - Import eslint-plugin-jest package, which removes warnings from test files.
    - Fix all lint errors.

--- part5/blogilistan-frontend/src/components/Error.js
+++ part5/blogilistan-frontend/src/components/Error.js
@@ -1,4 +1,4 @@
-const Error = ({message}) => {
+const Error = ({ message }) => {
   if (message === null) {
     return null
   }
--- part5/blogilistan-frontend/src/components/Notification.js
+++ part5/blogilistan-frontend/src/components/Notification.js
@@ -1,4 +1,4 @@
-const Notification = ({message}) => {
+const Notification = ({ message }) => {
   if (message === null) {
     return null
   }
