40c8d612c619abec092c196ea7c25335e09d9c4e
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-10 12:59:30 +0200

    Complete exercise 5.14
    - Import user-event package.
    - Add test for displaying url and likes when show button is pressed.
    - Refactor Blog test code to reduce repetition by using the same blog values in every test and rendering Blog automatically before each test.

--- part5/blogilistan-frontend/src/components/Blog.test.js
+++ part5/blogilistan-frontend/src/components/Blog.test.js
@@ -1,35 +1,57 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
+describe('<Blog /> ', () => {
+  const title = 'Testing blog title'
+  const author = 'Testing blog author'
+  const url = 'Testing blog url'
+  const likes = 10
+
   const blog = {
-    title: 'Testing blog title',
-    author: 'Testing blog author',
+    title,
+    author,
+    url,
+    likes,
   }
 
-  render(<Blog blog={blog} />)
-
-  screen.getByText('Testing blog title', { exact: false })
-  screen.getByText('Testing blog author', { exact: false })
-})
-
-test('hides url and likes at start', () => {
-  const blog = {
-    title: 'Testing blog title',
-    author: 'Testing blog author',
-    url: 'Testing blog url',
-    likes: 10,
+  const user = {
+    username: 'Alias',
   }
 
-  render(<Blog blog={blog} />)
+  beforeEach(() => {
+    render(
+      <Blog
+        blog={blog}
+        user={user}
+        deleteBlog={() => {}}
+        updateBlog={() => {}}
+      />
+    ).container
+  })
 
-  screen.debug()
+  test('renders title and author', () => {
+    screen.getByText(title, { exact: false })
+    screen.getByText(author, { exact: false })
+  })
 
-  const urlElement = screen.queryByText('Testing blog url')
-  expect(urlElement).toBeNull()
+  test('hides url and likes at start', () => {
+    const urlElement = screen.queryByText(url)
+    expect(urlElement).toBeNull()
 
-  const likesElement = screen.queryByText(10)
-  expect(likesElement).toBeNull()
+    const likesElement = screen.queryByText(10)
+    expect(likesElement).toBeNull()
+  })
+
+  test('clicking the show button displays url and likes', async () => {
+    const user = userEvent.setup()
+
+    const button = screen.getByText('show')
+    await user.click(button)
+
+    screen.getByText(url)
+    screen.getByText(likes, { exact: false })
+  })
 })
