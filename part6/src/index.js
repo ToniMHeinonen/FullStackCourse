5e0a62d70b4fc6fb635730bec686aad84c43867e
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-11 14:16:27 +0200

    Add part 6 react-redux examples
    - Import react-redux package.
    - Separate code from index.js to multiple different files.
    - Use NewNote and Notes components in App.js to render creating new notes and displaying all created notes.

--- part6/src/index.js
+++ part6/src/index.js
@@ -1,47 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createStore } from 'redux'
+import App from './App'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1,
-  },
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2,
-  },
-})
-
-const App = () => {
-  return (
-    <div>
-      <ul>
-        {store.getState().map((note) => (
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
-
-const renderApp = () => {
-  ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
--- part6/src/reducers/noteReducer.js
+++ part6/src/reducers/noteReducer.js
@@ -15,4 +15,24 @@ const noteReducer = (state = [], action) => {
   }
 }
 
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  }
+}
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    data: { id },
+  }
+}
+
 export default noteReducer
