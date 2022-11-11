f55284a85552d1e3af25e62d171c77af1f24e8c8
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-11 12:53:33 +0200

    Add part 6 Redux basic examples
    - Import redux package and deep-freeze package as development package.
    - Add noteReducer reducer and test file for it.
    - Render noteReducer in index.js.

--- part6/src/index.js
+++ part6/src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createStore } from 'redux'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import noteReducer from './reducers/noteReducer'
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1,
+  },
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2,
+  },
+})
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
