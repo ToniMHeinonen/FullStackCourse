367746f1d59382f5985f5169f70c3957beee4fe8
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-11 09:20:20 +0200

    Complete exercise 5.18
    - Add one user before each cypress test.
    - Test login with correct and wrong credentials.

--- part5/blogilistan-frontend/cypress/e2e/blog_app.cy.js
+++ part5/blogilistan-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'User Agent',
+      username: 'user123',
+      password: 'password123',
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +15,26 @@ describe('Blog app', function () {
     cy.get('#login-username')
     cy.get('#login-password')
     cy.get('#login-submit')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#login-username').type('user123')
+      cy.get('#login-password').type('password123')
+      cy.get('#login-submit').click()
+      cy.contains('User Agent logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#login-username').type('user')
+      cy.get('#login-password').type('password')
+      cy.get('#login-submit').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.get('html').should('not.contain', 'User Agent logged in')
+    })
   })
 })
