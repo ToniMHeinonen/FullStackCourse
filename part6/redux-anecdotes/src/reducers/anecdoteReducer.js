e3d201ed58f762e5da763d84e0fef998c1e04bf2
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-14 10:57:18 +0200

    Complete exercise 6.9
    - Import Redux Toolkit package.
    - Add store.js and configure Redux store in there.
    - Modify AnecdoteList component to work with Redux Toolkit.

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,7 +19,7 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'VOTE_ANECDOTE':
       const anecdote = state.find((a) => a.id === action.data.id)
@@ -53,4 +53,4 @@ export const createAnecdote = (content) => {
   }
 }
 
-export default reducer
+export default anecdoteReducer
