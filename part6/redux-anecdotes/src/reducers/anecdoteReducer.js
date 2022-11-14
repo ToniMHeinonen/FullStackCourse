ed0d9e95cd4de5b0c8d23e4de38b880c01d8fe44
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-14 09:22:48 +0200

    Complete exercise 6.3
    - Save anecdote vote points to redux-store.
    - Add voteAnecdote() to anecdoteReducer.
    - Dispatch vote points with voteAnecdote() in App.js.

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -4,7 +4,7 @@ const anecdotesAtStart = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
@@ -13,17 +13,28 @@ const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
-    votes: 0
+    votes: 0,
   }
 }
 
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch (action.type) {
+    case 'VOTE_ANECDOTE':
+      const anecdote = state.find((a) => a.id === action.data.id)
+      const modified = { ...anecdote, votes: anecdote.votes + 1 }
+      return state.map((a) => (a.id !== modified.id ? a : modified))
+    default:
+      return state
+  }
+}
 
-  return state
+export const voteAnecdote = (id) => {
+  return {
+    type: 'VOTE_ANECDOTE',
+    data: { id },
+  }
 }
 
 export default reducer
