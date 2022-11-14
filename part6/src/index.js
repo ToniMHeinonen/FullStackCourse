56376751c549eaf8917cd3bfb1dadccbe9b61574
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-14 10:47:37 +0200

    Add part 6 Redux Toolkit examples
    - Import Redux Toolkit package.
    - Add VisibilityFilter component and filterReducer for filtering notes by importance.
    - Modify index.js file to use Redux Toolkit for controlling multiple reducers.
    - Convert noteReducer to use Redux Toolkit slices.
    - Filter notes by importance in Notes component.
    - Fix noteReducer tests to function with the Redux Toolkit correctly.
    - Remove App.test.js since it is not needed.

--- part6/src/index.js
+++ part6/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
 
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
+})
+
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 )
