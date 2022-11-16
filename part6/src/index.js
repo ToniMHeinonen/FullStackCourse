13a4cc6ac5ccbc86d7e8ff5ad9d46e8341d7407a
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-16 09:24:23 +0200

    Add part 6 Redux Thunk examples
    - Import redux-thunk package.
    - Move noteService code from App.js and NewNote.js to noteReducer.js.
    - Move redux configuration from index.js to store.js.

--- part6/src/index.js
+++ part6/src/index.js
@@ -1,20 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
+import store from './store'
 import App from './App'
-
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-})
-
-console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- part6/src/reducers/noteReducer.js
+++ part6/src/reducers/noteReducer.js
@@ -1,12 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
+import noteService from '../services/notes'
 
 const noteSlice = createSlice({
   name: 'notes',
   initialState: [],
   reducers: {
-    createNote(state, action) {
-      state.push(action.payload)
-    },
     toggleImportanceOf(state, action) {
       const id = action.payload
       const noteToChange = state.find((n) => n.id === id)
@@ -25,6 +23,21 @@ const noteSlice = createSlice({
   },
 })
 
-export const { createNote, toggleImportanceOf, appendNote, setNotes } =
-  noteSlice.actions
+export const { toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+
+// These functions require redux-thunk library
+export const initializeNotes = () => {
+  return async (dispatch) => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const createNote = (content) => {
+  return async (dispatch) => {
+    const newNote = await noteService.createNew(content)
+    dispatch(appendNote(newNote))
+  }
+}
+
 export default noteSlice.reducer
