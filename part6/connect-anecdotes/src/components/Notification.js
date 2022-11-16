85c0ce74ec76a94feca1e58ba7bfa0d6e2d6c920
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-16 13:14:08 +0200

    Complete exercise 6.19
    Replace hook-api calls with Redux connect function in Notification component.

--- part6/connect-anecdotes/src/components/Notification.js
+++ part6/connect-anecdotes/src/components/Notification.js
@@ -1,7 +1,7 @@
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector((state) => state.notification)
+const Notification = (props) => {
+  const notification = props.notification
   const style = {
     border: 'solid',
     padding: 10,
@@ -13,4 +13,10 @@ const Notification = () => {
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification,
+  }
+}
+
+export default connect(mapStateToProps, null)(Notification)
