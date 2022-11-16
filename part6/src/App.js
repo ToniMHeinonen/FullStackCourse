2bfa43dc51f03915a5f9af6421e8f5e9c7714ef5
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-16 08:47:40 +0200

    Add part6 json server with redux examples
    - Import axios and json-server packages.
    - Add db.json file to root of project.
    - Add notes service, which controls getting all notes from server and posting new notes.
    - Modify other files to support json-server.

--- part6/src/App.js
+++ part6/src/App.js
@@ -1,8 +1,17 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
+import { setNotes } from './reducers/noteReducer'
+import noteService from './services/notes'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    noteService.getAll().then((notes) => dispatch(setNotes(notes)))
+  }, [dispatch])
+
   return (
     <div>
       <NewNote />
