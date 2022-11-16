70b7d776f1d1cd6193d3ecf59941e0b6eda1b8e1
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-16 12:29:37 +0200

    Copy part6 to part6-connect directory
    Part 6 is going to be modified to use the connect function, which is a lot worse than the current hook api and is used in older Redux projects. It is better to separate it to other directory, so both versions are easily available.

---
+++ part6/part6-connect/src/App.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import NewNote from './components/NewNote'
+import Notes from './components/Notes'
+import VisibilityFilter from './components/VisibilityFilter'
+import { initializeNotes } from './reducers/noteReducer'
+
+const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeNotes())
+  }, [dispatch])
+
+  return (
+    <div>
+      <NewNote />
+      <VisibilityFilter />
+      <Notes />
+    </div>
+  )
+}
+
+export default App
