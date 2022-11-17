e093bea62007c89cc9e9d7e00e0772b47dd648d0
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-17 12:39:59 +0200

    Complete exercise 7.7
    Use custom hook for retrieving country information which uses useEffect().

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -11,14 +11,23 @@ const useField = (type) => {
   return {
     type,
     value,
-    onChange
+    onChange,
   }
 }
 
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (!name) return
+
+    axios
+      .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      .then((country) => {
+        setCountry(country.data[0])
+      })
+      .catch((exception) => setCountry(null))
+  }, [name])
 
   return country
 }
@@ -31,11 +40,15 @@ const Country = ({ country }) => {
   return (
     <div>
       <h3>{country.name.common}</h3>
-      <div>population {country.population}</div> 
+      <div>population {country.population}</div>
       <div>capital {country.capital}</div>
-      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
+      <img
+        src={country.flags.png}
+        height="100"
+        alt={`flag of ${country.name.common}`}
+      />
     </div>
-  )  
+  )
 }
 
 const App = () => {
