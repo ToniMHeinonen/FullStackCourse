2e33f5828a323adc4e9daa47bc44d5acbefaede6
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-22 11:53:40 +0200

    Add part 7 class examples
    - Add class example in App.js.
    - Add commented out example of hook version of the same App.

--- part7/example-classes/src/App.js
+++ part7/example-classes/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      anecdotes: [],
+      current: 0,
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get('http://localhost:3001/anecdotes').then((response) => {
+      this.setState({ anecdotes: response.data })
+    })
+  }
+
+  handleClick = () => {
+    const current = Math.floor(
+      Math.random() * (this.state.anecdotes.length - 1)
+    )
+    this.setState({ current })
+  }
+
+  render() {
+    if (this.state.anecdotes.length === 0) {
+      return <div>no anecdotes...</div>
+    }
+
+    return (
+      <div>
+        <h1>anecdote of the day</h1>
+        <div>{this.state.anecdotes[this.state.current].content}</div>
+        <button onClick={this.handleClick}>next</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+/* Hook version
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([])
+  const [current, setCurrent] = useState(0)
+
+  useEffect(() =>{
+    axios.get('http://localhost:3001/anecdotes').then(response => {
+      setAnecdotes(response.data)
+    })
+  },[])
+
+  const handleClick = () => {
+    setCurrent(Math.round(Math.random() * (anecdotes.length - 1)))
+  }
+
+  if (anecdotes.length === 0) {
+    return <div>no anecdotes...</div>
+  }
+
+  return (
+    <div>
+      <h1>anecdote of the day</h1>
+      <div>{anecdotes[current].content}</div>
+      <button onClick={handleClick}>next</button>
+    </div>
+  )
+}
+*/
+
+export default App
