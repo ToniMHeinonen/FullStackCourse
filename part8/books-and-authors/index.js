540d380cfdda547a882b91cff707238c193fc211
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-22 13:57:16 +0200

    Complete exercise 8.4
    - Add optional author parameter to allBooks() query.
    - Retrieve books by author in allBooks() query if author is provided.

--- part8/books-and-authors/index.js
+++ part8/books-and-authors/index.js
@@ -111,7 +111,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -120,7 +120,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) return books
+
+      const byAuthor = (book) => book.author === args.author
+      return books.filter(byAuthor)
+    },
     allAuthors: () => authors,
   },
   Author: {
