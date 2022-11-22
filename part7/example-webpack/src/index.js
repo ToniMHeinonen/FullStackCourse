0ea4f48797c0654f7cad27a07d12f8f67162357e
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-22 09:49:33 +0200

    Add part 7 webpack examples
    - Add examples for configuring React app by using webpack instead of create-react-app command.
    - Add loaders which handle different file types correctly before bundling.
    - Add preset-env which transpiles the code to ES5 standard.
    - Add webpack-dev-server which automatically refreshes changes on browser.
    - Use source-map which shows the correct path to error messages.
    - Minify bundled code with UglifyJS by setting build mode to production.
    - Use promise-polyfill for adding support to Internet Explorer promises.

---
+++ part7/example-webpack/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+import './index.css'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />,
+  document.getElementById('root')
+)
