4891536ba77738a07aa8247933c04c7e239fa739
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-22 13:50:49 +0200

    Complete exercise 8.3
    - Add bookCount field to Author type.
    - Add allAuthors() query.
    - Add resolver for Author, which resolves bookCount.
    - Prevent null returns on Book type genres and allBooks() query.

--- part8/books-and-authors/index.js
+++ part8/books-and-authors/index.js
@@ -97,20 +97,22 @@ const typeDefs = gql`
     title: String!
     published: Int!
     author: String!
-    genres: [String!]
+    genres: [String!]!
     id: ID!
   }
 
   type Author {
     name: String!
     born: Int!
+    bookCount: Int!
     id: ID!
   }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -119,6 +121,10 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: ({ name }) => books.filter((b) => b.author === name).length,
   },
 }
 
