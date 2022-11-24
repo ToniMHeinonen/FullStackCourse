be5424d3c710bb040458e80291cf5bde5226817b
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-24 09:40:01 +0200

    Complete exercise 8.8
    - Import @apollo/client and graphql packages.
    - Configure Apollo client in index.js.
    - Create queries.js and add ALL_AUTHORS query there.
    - Use ALL_AUTHORS query in Authors component.

--- part8/library-frontend/src/index.js
+++ part8/library-frontend/src/index.js
@@ -1,5 +1,22 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  }),
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
