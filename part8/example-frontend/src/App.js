991aa2a12930927215d120e91b1c85f59f508112
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-25 12:28:20 +0200

    Add part 8 frontend authorization examples
    - Import apollo-link-context, which makes it possible to pass user token in header when using ApolloClient.
    - Configure authorization in index.js.
    - Add LoginForm component.
    - Make it possible to create new person without phone number.
    - Update cache when creating new persons instead of fetching all persons.
    - Add LOGIN query.
    - Show LoginForm if user is not logged in.
    - Reset cache when user logs out.

--- part8/example-frontend/src/App.js
+++ part8/example-frontend/src/App.js
@@ -1,18 +1,16 @@
-import { useQuery } from '@apollo/client'
+import { useApolloClient, useQuery } from '@apollo/client'
 import { useState } from 'react'
+import LoginForm from './components/LoginForm'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import PhoneForm from './components/PhoneForm'
 import { ALL_PERSONS } from './queries'
 
 const App = () => {
+  const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
-
   const result = useQuery(ALL_PERSONS)
-
-  if (result.loading) {
-    return <div>loading...</div>
-  }
+  const client = useApolloClient()
 
   const notify = (message) => {
     setErrorMessage(message)
@@ -21,13 +19,33 @@ const App = () => {
     }, 10000)
   }
 
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <>
+        <Notify errorMessage={errorMessage} />
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
+    )
+  }
+
   return (
-    <div>
+    <>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
-    </div>
+    </>
   )
 }
 
