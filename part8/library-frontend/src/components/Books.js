67eb14b98014156f2e96793a2f011c3b00b29a3a
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-11-25 13:53:40 +0200

    Complete exercise 8.19
    - Add genres field to ALL_BOOKS query.
    - Add ability to display books by genre.
    - Retrieve all genres for books by using reduce function and Set.

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,7 +1,10 @@
 import { useQuery } from '@apollo/client'
+import { useState } from 'react'
 import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
+  const allGenres = 'all genres'
+  const [genre, setGenre] = useState(allGenres)
   const result = useQuery(ALL_BOOKS)
 
   if (!props.show) {
@@ -12,11 +15,38 @@ const Books = (props) => {
     return <div>loading...</div>
   }
 
+  const selectedStyle = {
+    backgroundColor: 'lightblue',
+  }
+
   const books = result.data.allBooks
+
+  const genres = books.reduce((currentGenres, book) => {
+    return [...new Set([...currentGenres, ...book.genres])]
+  }, [])
+
+  genres.push(allGenres)
+
+  const showBook = (book) => {
+    if (genre === allGenres || book.genres.includes(genre)) {
+      return (
+        <tr key={book.title}>
+          <td>{book.title}</td>
+          <td>{book.author.name}</td>
+          <td>{book.published}</td>
+        </tr>
+      )
+    }
+
+    return null
+  }
 
   return (
     <div>
       <h2>books</h2>
+      <p>
+        in genre <strong>{genre}</strong>
+      </p>
 
       <table>
         <tbody>
@@ -25,15 +55,20 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books.map((b) => showBook(b))}
         </tbody>
       </table>
+      <div>
+        {genres.map((g) => (
+          <button
+            style={g === genre ? selectedStyle : {}}
+            key={g}
+            onClick={() => setGenre(g)}
+          >
+            {g}
+          </button>
+        ))}
+      </div>
     </div>
   )
 }
