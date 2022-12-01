47917e289cfd1f9eb57ec65707af874dcb945c91
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-12-01 11:53:08 +0200

    Complete exercise 9.11
    - Add Patient and NonSensitivePatient types.
    - Add patients.ts to data folder which holds all patient entries.
    - Add patients.ts router which controls patients routes.
    - Add patientSercice.ts  which controls handling of patients data.
    - Use patients router in index.ts at api/patients endpoint.

---
+++ part9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,22 @@
+import patients from '../../data/patients';
+
+import { NonSensitivePatient, Patient } from '../types';
+
+const getEntries = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitiveEntries = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default {
+  getEntries,
+  getNonSensitiveEntries,
+};
