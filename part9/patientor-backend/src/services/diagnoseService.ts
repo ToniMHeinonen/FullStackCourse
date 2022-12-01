d49aa15aa37a92a8bf7b02639138910e5f17f5df
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-12-01 11:38:25 +0200

    Complete exercise 9.10
    - Add types.ts for holding all app types, add Diagnose interface there.
    - Add data folder and add diagnoses.ts there, which holds diagnose entries.
    - Add src folder and move index.ts there.
    - Add src/routes folder and add diagnoses.ts there, which controls diagnose routes.
    - Add src/services folder and add diagnoseService.ts there, which controls handling of diagnose entry data.
    - Use diagnoseRouter in index.ts.

---
+++ part9/patientor-backend/src/services/diagnoseService.ts
@@ -0,0 +1,11 @@
+import diagnoses from '../../data/diagnoses';
+
+import { Diagnose } from '../types';
+
+const getEntries = (): Diagnose[] => {
+  return diagnoses;
+};
+
+export default {
+  getEntries,
+};
