3565fe3782c44169f0537ed9eb0dd3913a181c42
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-12-02 09:58:42 +0200

    Add part 9 interface extends examples
    - Copy index.tsx and App.tsx files from ../react-javascript project to this project.
    - Add ability to use different types in each course.
    - Add assertNever() which is used for checking that all switch statement values are handled.

--- part9/example-react/src/App.tsx
+++ part9/example-react/src/App.tsx
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+interface HeaderProps {
+  text: string;
+}
 
-function App() {
+interface ContentProps {
+  name: string;
+  exerciseCount: number;
+}
+
+interface TotalProps {
+  courseParts: CoursePartBase[];
+}
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartOne extends CoursePartBase {
+  name: 'Fundamentals';
+  description: string;
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: 'Using props to pass data';
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartBase {
+  name: 'Deeper type usage';
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree;
+
+/**
+ * Helper function for exhaustive type checking
+ */
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+const Header = (props: HeaderProps) => <h1>{props.text}</h1>;
+
+const Content = (props: ContentProps) => (
+  <p>
+    {props.name} {props.exerciseCount}
+  </p>
+);
+
+const Total = (props: TotalProps) => (
+  <p>
+    Number of exercises{' '}
+    {props.courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+  </p>
+);
+
+const App = () => {
+  const courseName = 'Half Stack application development';
+  const courseParts: CoursePart[] = [
+    {
+      name: 'Fundamentals',
+      exerciseCount: 10,
+      description: 'This is an awesome course part',
+    },
+    {
+      name: 'Using props to pass data',
+      exerciseCount: 7,
+      groupProjectCount: 3,
+    },
+    {
+      name: 'Deeper type usage',
+      exerciseCount: 14,
+      description: 'Confusing description',
+      exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev',
+    },
+  ];
+
+  courseParts.forEach((part) => {
+    switch (part.name) {
+      case 'Fundamentals':
+        // TypeScript knows that we can use name, exerciseCount and description
+        break;
+      case 'Using props to pass data':
+        // TypeScript knows that we can use name, exerciseCount and groupProjectCount
+        break;
+      case 'Deeper type usage':
+        // TypeScript knows that we can use name, exerciseCount, description and exerciseSubmissionLink
+        break;
+      default:
+        // Makes sure that all names are implemented in this switch statement
+        return assertNever(part);
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header text={courseName} />
+      {courseParts.map((c) => (
+        <Content
+          key={c.name}
+          name={c.name}
+          exerciseCount={c.exerciseCount}
+        ></Content>
+      ))}
+      <Total courseParts={courseParts} />
     </div>
   );
-}
+};
 
 export default App;
--- part9/example-react/src/index.tsx
+++ part9/example-react/src/index.tsx
@@ -1,19 +1,6 @@
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-interface WelcomeProps {
-  name: string;
-}
-
-const Welcome = (props: WelcomeProps) => {
-  return <h1>Hello, {props.name}</h1>;
-};
-
-const element = <Welcome name="Sara" />;
-ReactDOM.render(element, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <App />
+);
