f989be09ead41991730aa8a15e5bd9ff21547196
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-12-02 09:22:20 +0200

    Add part 9 TypeScript basic React app example
    - Create React app by using command "npx create-react-app example-react --template typescript".
    - Add .eslintrc file.
    - Add script "lint": "eslint \"./src/**/*.{ts,tsx}\"".
    - Set allowJS to false in tsconfig.json.
    - Add basic prop example to index.tsx.

---
+++ part9/example-react/src/index.tsx
@@ -0,0 +1,19 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+interface WelcomeProps {
+  name: string;
+}
+
+const Welcome = (props: WelcomeProps) => {
+  return <h1>Hello, {props.name}</h1>;
+};
+
+const element = <Welcome name="Sara" />;
+ReactDOM.render(element, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
