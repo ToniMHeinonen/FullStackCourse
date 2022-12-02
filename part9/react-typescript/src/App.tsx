90a9a9e4fe487849326a6635992bd4d8f1aed51f
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-12-02 10:35:12 +0200

    Complete exercise 9.15
    - Add different types of courses.
    - Extend other interfaces in course types in order to reduce duplication.
    - Add Part component, which renders all fields for course part.
    - Add assertNever() which makes sure that all switch statement values are handled.

--- part9/react-typescript/src/App.tsx
+++ part9/react-typescript/src/App.tsx
@@ -2,59 +2,180 @@ interface HeaderProps {
   text: string;
 }
 
-interface ContentProps {
-  name: string;
-  exerciseCount: number;
+interface CourseProps {
+  courseParts: CoursePart[];
 }
 
-interface TotalProps {
-  courseParts: ContentProps[];
+interface PartProps {
+  coursePart: CoursePart;
 }
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CoursePartBaseDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CoursePartBaseDescription {
+  type: 'normal';
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: 'groupProject';
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBaseDescription {
+  type: 'submission';
+  exerciseSubmissionLink: string;
+}
+
+interface CourseSpecialPart extends CoursePartBaseDescription {
+  type: 'special';
+  requirements: string[];
+}
+
+type CoursePart =
+  | CourseNormalPart
+  | CourseProjectPart
+  | CourseSubmissionPart
+  | CourseSpecialPart;
 
 const Header = (props: HeaderProps) => <h1>{props.text}</h1>;
 
-const Content = (props: ContentProps) => (
-  <p>
-    {props.name} {props.exerciseCount}
-  </p>
+const Content = (props: CourseProps) => (
+  <div>
+    {props.courseParts.map((c) => (
+      <Part key={c.name} coursePart={c} />
+    ))}
+  </div>
 );
 
-const Total = (props: TotalProps) => (
+const Total = (props: CourseProps) => (
   <p>
     Number of exercises{' '}
     {props.courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
   </p>
 );
 
+const Part = (props: PartProps) => {
+  const part = props.coursePart;
+  switch (part.type) {
+    case 'normal':
+      return (
+        <p>
+          <div>
+            <strong>
+              {part.name} {part.exerciseCount}
+            </strong>
+          </div>
+          <div>
+            <i>{part.description}</i>
+          </div>
+        </p>
+      );
+    case 'groupProject':
+      return (
+        <p>
+          <div>
+            <strong>
+              {part.name} {part.exerciseCount}
+            </strong>
+          </div>
+          <div>project exercises {part.groupProjectCount}</div>
+        </p>
+      );
+    case 'submission':
+      return (
+        <p>
+          <div>
+            <strong>
+              {part.name} {part.exerciseCount}
+            </strong>
+          </div>
+          <div>
+            <i>{part.description}</i>
+          </div>
+          <div>submit to {part.exerciseSubmissionLink}</div>
+        </p>
+      );
+    case 'special':
+      return (
+        <p>
+          <div>
+            <strong>
+              {part.name} {part.exerciseCount}
+            </strong>
+          </div>
+          <div>
+            <i>{part.description}</i>
+          </div>
+          <div>
+            required skills: {part.requirements.map((r) => r).join(', ')}
+          </div>
+        </p>
+      );
+    default:
+      return assertNever(part);
+  }
+};
+
 const App = () => {
   const courseName = 'Half Stack application development';
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
       exerciseCount: 10,
+      description: 'This is the easy course part',
+      type: 'normal',
+    },
+    {
+      name: 'Advanced',
+      exerciseCount: 7,
+      description: 'This is the hard course part',
+      type: 'normal',
     },
     {
       name: 'Using props to pass data',
       exerciseCount: 7,
+      groupProjectCount: 3,
+      type: 'groupProject',
     },
     {
       name: 'Deeper type usage',
       exerciseCount: 14,
+      description: 'Confusing description',
+      exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev',
+      type: 'submission',
+    },
+    {
+      name: 'Backend development',
+      exerciseCount: 21,
+      description: 'Typing the backend',
+      requirements: ['nodejs', 'jest'],
+      type: 'special',
     },
   ];
 
   return (
     <div>
       <Header text={courseName} />
-      {courseParts.map((c) => (
-        <Content
-          key={c.name}
-          name={c.name}
-          exerciseCount={c.exerciseCount}
-        ></Content>
-      ))}
+      <Content courseParts={courseParts} />
       <Total courseParts={courseParts} />
     </div>
+  );
+};
+
+/**
+ * Helper function for exhaustive type checking
+ */
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
   );
 };
 
