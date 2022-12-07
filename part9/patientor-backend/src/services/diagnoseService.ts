8693447747851985176b993a3eeafe491d471abb
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-12-07 09:40:38 +0200

    Add part 9 Entry type interface extends examples
    - Add entries to patients.ts data.
    - Rename Diagnose interface to Diagnosis.
    - Add BaseEntry and HealthCheckEntry types.
    - Change Entry interface to union type.
    - Add HealtCheckRating enum.

--- part9/patientor-backend/src/services/diagnoseService.ts
+++ part9/patientor-backend/src/services/diagnoseService.ts
@@ -1,8 +1,8 @@
 import diagnoses from '../../data/diagnoses';
 
-import { Diagnose } from '../types';
+import { Diagnosis } from '../types';
 
-const getEntries = (): Diagnose[] => {
+const getEntries = (): Diagnosis[] => {
   return diagnoses;
 };
 
--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,17 +1,39 @@
-export interface Diagnose {
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
 }
 
 export enum Gender {
-  male = 'male',
-  female = 'female',
-  other = 'other',
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  //  | HospitalEntry
+  //  | OccupationalHealthcareEntry
+  HealthCheckEntry;
 
 export interface Patient {
   id: string;
