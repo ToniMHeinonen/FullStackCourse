4ed134beb0d438fecc61ac4b78707af01c092e68
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-12-12 13:10:17 +0200

    Add part 10 Routing examples
    - Install react-router-native package.
    - Setup NativeRouter in App.js.
    - Define Routes in Main component and catch all wrong paths point to home path.

--- part10/rate-repository-app/src/components/Main.jsx
+++ part10/rate-repository-app/src/components/Main.jsx
@@ -1,4 +1,6 @@
 import { StyleSheet, View } from 'react-native'
+import { Route, Routes, Navigate } from 'react-router-native'
+
 import theme from '../theme'
 import AppBar from './AppBar'
 import RepositoryList from './RepositoryList'
@@ -15,7 +17,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} exact />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   )
 }
